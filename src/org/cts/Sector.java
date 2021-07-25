3210e8695e8501d96b230545d420d46d3357efb3
janani489 <siriyalamarthi4177@gmail.com>
2021-07-25 16:22:34 +0530

    valli task is done

--- src/org/cts/Sector.java
+++ src/org/cts/Sector.java
@@ -1,7 +1,14 @@
 package org.cts;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class Sector {
 	public static void main(String[] args) {
 		System.out.println("java");
+
+		List<String> s = new LinkedList<>();
+		s.add("valli");
+
 	}
 }
